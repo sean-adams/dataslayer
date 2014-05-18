390f8cb60e98f9ac47932688e27a0a42f0f3f514
Sean Adams <sean.adams.86@gmail.com>
2014-05-18 16:36:45 -0400

    stop throwing content-side errors when content.js has lost the devtools panel that injected it

--- src/content.js
+++ src/content.js
@@ -1,7 +1,14 @@
 var dataslayer = {};
 dataslayer.helperListener = function(event){
 	if ((event.source == window)&&(event.data.type && (event.data.type=='dataslayer_gtm'))){
-		chrome.runtime.sendMessage(event.data);
+		try{
+			chrome.runtime.sendMessage(event.data);
+		}
+		catch(e){
+			// nothing to be done here, really, as generally an error here 
+			// means we have lost a previous version of the devtools panel
+			// that initially injected this iteration of the content script
+		}
 	}
 };
 window.addEventListener('message',dataslayer.helperListener);
