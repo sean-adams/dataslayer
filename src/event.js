8f91542826577f1a4de34936d514c736eba28439
Sean Adams <sean@morpheusmedia.com>
2014-07-22 11:50:31 -0400

    only attempt to add blocking if it is available

--- src/event.js
+++ src/event.js
@@ -8,36 +8,38 @@ chrome.runtime.onConnect.addListener(function(port){
 
 function addBlocking(){
 	removeBlocking();
-	chrome.declarativeWebRequest.onRequest.addRules([{
-		id: 'dataslayerBlocking',
-		conditions: [
-			new chrome.declarativeWebRequest.RequestMatcher({
-				url: { hostSuffix: 'google-analytics.com', pathPrefix: '/collect', schemes: ['http','https'] },
-			}),
-			new chrome.declarativeWebRequest.RequestMatcher({
-				url: { hostSuffix: 'google-analytics.com', pathPrefix: '/__utm.gif', schemes: ['http','https'] },
-			}),
-			new chrome.declarativeWebRequest.RequestMatcher({
-				url: { hostSuffix: 'stats.g.doubleclick.net', pathPrefix: '/__utm.gif', schemes: ['http','https'] },
-			}),
-			new chrome.declarativeWebRequest.RequestMatcher({
-				url: { hostSuffix: 'doubleclick.net', pathPrefix: '/activity', schemes: ['http','https'] },
-			}),
-			new chrome.declarativeWebRequest.RequestMatcher({
-				url: { pathPrefix: '/b/ss', queryContains: 'AQB=1', schemes: ['http','https'] },
-			})
-			],
-		actions: [
-			new chrome.declarativeWebRequest.RedirectToTransparentImage()
-		]}]);
+	if (chrome.declarativeWebRequest)
+		chrome.declarativeWebRequest.onRequest.addRules([{
+			id: 'dataslayerBlocking',
+			conditions: [
+				new chrome.declarativeWebRequest.RequestMatcher({
+					url: { hostSuffix: 'google-analytics.com', pathPrefix: '/collect', schemes: ['http','https'] },
+				}),
+				new chrome.declarativeWebRequest.RequestMatcher({
+					url: { hostSuffix: 'google-analytics.com', pathPrefix: '/__utm.gif', schemes: ['http','https'] },
+				}),
+				new chrome.declarativeWebRequest.RequestMatcher({
+					url: { hostSuffix: 'stats.g.doubleclick.net', pathPrefix: '/__utm.gif', schemes: ['http','https'] },
+				}),
+				new chrome.declarativeWebRequest.RequestMatcher({
+					url: { hostSuffix: 'doubleclick.net', pathPrefix: '/activity', schemes: ['http','https'] },
+				}),
+				new chrome.declarativeWebRequest.RequestMatcher({
+					url: { pathPrefix: '/b/ss', queryContains: 'AQB=1', schemes: ['http','https'] },
+				})
+				],
+			actions: [
+				new chrome.declarativeWebRequest.RedirectToTransparentImage()
+			]}]);
 }
 
 function removeBlocking(){
-	chrome.declarativeWebRequest.onRequest.removeRules(['dataslayerBlocking']);
+	if (chrome.declarativeWebRequest)
+		chrome.declarativeWebRequest.onRequest.removeRules(['dataslayerBlocking']);
 }
 
 chrome.storage.sync.get(null,function(items){
-	if (ourItems.hasOwnProperty('blockTags')&&ourItems.blockTags===true) addBlocking();
+	if (items.hasOwnProperty('blockTags')&&items.blockTags===true) addBlocking();
 	else removeBlocking();
 });
 
