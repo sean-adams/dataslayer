55925ef92ba05c89a79e833145406ca4c9e84f70
Sean Adams <sean@bearcla.ws>
2019-06-05 00:02:20 -0400

    fix panel name

--- public/dataslayer.js
+++ public/dataslayer.js
@@ -1,7 +1,7 @@
 /* global chrome */
 
 chrome.devtools.panels.create(
-    'dataslayer' + (chrome.runtime.id === 'ikbablmmjldhamhcldjjigniffkkjgpo' ? '' : ' beta'),
+    'dataslayer' + (chrome.runtime.id === 'ikbablmmjldhamhcldjjigniffkkjgpo' || chrome.runtime.id === 'firefox@dataslayer.org' ? '' : ' beta'),
     'i128.png', // No icon path
     'index.html',
     null // no callback needed
