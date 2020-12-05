a8e497321b435aa7dd3a29805d238c8779a9e955
sean <sean@x260.myfiosgateway.com>
2020-12-04 22:51:04 -0500

    increment version number

--- src/Options.js
+++ src/Options.js
@@ -7,7 +7,7 @@ import { isChrome, isFirefox } from './helpers';
 class Options extends Component {
   constructor(props) {
     super(props);
-    let version = '1.3.7';
+    let version = '1.3.8';
     if (typeof chrome !== 'undefined' && typeof chrome.runtime !== 'undefined' && typeof chrome.runtime.getManifest !== 'undefined') {
       version = chrome.runtime.getManifest().version;
     }
